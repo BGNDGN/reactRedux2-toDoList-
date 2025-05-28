75f902f855d733ce9c6271dd0e667ea4c0f74f39
Burak <148650898+BGNDGN@users.noreply.github.com>
2025-05-28 16:33:02 +0300

    Update todoSlice.js

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -4,18 +4,9 @@ const initialState = {
 
 const todoReducer = (state = initialState, action) => {
   switch (action.type) {
-    case "ADD_TODO":
-      return {
-        ...state,
-        list: [...state.list, action.payload],
-      };
-    case "REMOVE_TODO":
-      return {
-        ...state,
-        list: state.list.filter((_, i) => i !== action.payload),
-      };
-    default:
-      return state;
+    case "ADD_TODO": return {...state, list: [...state.list, action.payload],};
+    case "REMOVE_TODO": return {...state, list: state.list.filter((_, i) => i !== action.payload),};
+    default: return state;
   }
 };
 
